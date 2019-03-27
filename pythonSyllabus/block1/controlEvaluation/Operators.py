b4b7ff4e69abc4b9f856f3939847b4f0dd3a59f2
yorland Garcia <yorland.garcia@imagemaker.cl>
2019-03-26 21:07:37 -0300

    Operators: Unary Operators and Binary Operators both with thier priorities

---
+++ pythonSyllabus/block1/controlEvaluation/Operators.py
@@ -0,0 +1,28 @@
+# Unary Operators
+
+# Operators Priority
+# P   Parentheses, then ()
+# E   Exponents, then (**)
+# MD  Multiplication and division, left to right, then (* /  //)
+# AS  Addition and subtraction, left to right (+ -)
+
+print ("This is the priority order, from highest to lessest: () ,", "** ,", "// ,", "%  *  / (Left to right) ,",
+       "+ - (Left to right)")
+print ("\n (2+5) + 2*3/3 ** 2 = ")
+print("\n 7 + 6 / 9")
+print("\n 7 + 0.67")
+print("\n =", (2+5) + 2*3/3 ** 2)
+
+# From left to right
+print("\n 2 ** 2 ** 3 =", 2 ** 2 ** 3)
+
+
+print("\n 2 ** 3 = ", 2 ** 3)
+print("\n 2. ** 3. =", 2. ** 3.)
+print("\n 2. * 3. =", 2. * 3.)
+
+# Binary Operators
+print("Binary Operators:", "<, >, >=, <=, !=, ==, and, or, not")
+print("The run order is from left to right 5 > 4 and 5 == 6 or 5 > 4) :", 5 > 4 and 5 == 6 or 5 > 4)
+print("The other way round 5 > 4 or 5 > 6 and 5 == 6: ", 5 > 4 or 5 > 6 and 5 == 6)
+
